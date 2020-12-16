a396034bc97de3ccbbfcf20092ca9bb0f6b0670f
jssalazar <jonah95419@gmail.com>
2020-12-16 12:45:49 -0500

    Estructura home
    se agregar actividad publicaciones

--- app/src/main/java/com/des/galtest/ui/HomeActivity.java
+++ app/src/main/java/com/des/galtest/ui/HomeActivity.java
@@ -1,35 +1,21 @@
 package com.des.galtest.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.EditText;
-
 import com.des.galtest.R;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
-    EditText  mensaje;
-    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        mensaje=findViewById(R.id.editTextTextMultiLine);
-        mAuth=FirebaseAuth.getInstance();
 
+        findViewById(R.id.btnPublicaicones)
+                .setOnClickListener( v -> startActivity(new Intent(this, PublicacionesActivity.class)));
     }
-    @Override
-    protected void onStart(){
-        super.onStart();
-        FirebaseUser currentUser=mAuth.getInstance().getCurrentUser();
-        if(currentUser !=null){
-            String email=currentUser.getEmail();
-            String  name=currentUser.getDisplayName();
-            mensaje.setText("Correo"+email+"  Nombre:"+name);
-        }
 
-
-    }
 }
---
+++ app/src/main/java/com/des/galtest/ui/PublicacionesActivity.java
@@ -0,0 +1,16 @@
+package com.des.galtest.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.des.galtest.R;
+
+public class PublicacionesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publicaciones);
+    }
+}
